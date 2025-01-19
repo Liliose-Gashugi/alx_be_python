183a2a182bc36dc2b7a177894424a0c621dfd59e
Liliose-Gashugi <gashugililiose@gmail.com>
2025-01-19 16:20:44 +0200

    Simple Bank Account script

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,33 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        """Initialize the bank account with an optional initial balance"""
+
+        self.__account_balance = initial_balance
+
+
+        def deposit(self, amount):
+            """Deposit a specified amount into the account"""
+
+            if amount > 0:
+                self.__acccount_balance += amount
+            else:
+                print("Deposit must be positive.")
+
+        def withdraw(self, amount):
+            """Withdraw a specified amount if sufficient funds exist."""
+
+            if amount > 0:
+                if amount <= self.__account_balance:
+                    self.__account_balance -= amount
+                    return True
+                else:
+                        return False
+            else:
+                    print("Withdrawal must be positive.")
+                    return False
+        
+
+        def display_balance(self):
+             """Display the current balance."""
+             print(f"Current Balance: ${self.__accoun_balance:.2f}")
+
